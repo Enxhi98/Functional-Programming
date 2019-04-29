eeef8caafaddc9ab718f4b2f1b642cec95abab75
Enxhi <21321344@student.uwl.ac.uk>
2019-04-29 12:43:02 +0100

    Add files via upload

--- personal.hs
+++ personal.hs
@@ -5,7 +5,9 @@ type Location = String
 type Direction = String
 type Thing = String
 type Response = String
+type Enemy = String
 
+--Map
 type PathMap = [((Location, Direction), Location)]
 paths :: PathMap
 paths = [
@@ -18,35 +20,46 @@ paths = [
     (("whiteRoom", "c"), "whiteRoom1")
     ]
 
+--Locations
 type LocationMap = [(Thing, Location)]
 locations :: LocationMap
 locations =  [
-    ("myself", "gameStart")
-    ("myself", "greenRoom"),
-    ("myself", "whiteRoom")
-    ("diamond", "whiteRoom"),
+    ("myself", "gameStart"),
+    --("myself", "greenRoom"),
+    --("myself", "whiteRoom"),
+    ("diamond", "holding"),
     ("sword", "redRoom"),
-    ("fireball", "blueRoom"),
-    ("hands", "blackRoom"),
-    ("giant", "redRoom"),
-    ("zombie", "blueRoom"),
-    ("spider", "blackRoom"),
-    ("dead", "greenRoom1"),
-    ("holding", "whiteRoom"),
-    ("alive", "whiteRoom")
-    ]
+    ("greenRoomAlive", "alive"),
+    ("holding", "whiteRoom")
 
-type World = (PathMap, LocationMap, Response)
-world :: IO (PathMap, LocationMap, Response)
-world = return (paths, locations, "")
+    ]
+--Inventory of weapons
+type Inventory = [Thing]
+inventory :: Inventory
+inventory = []
+
+--Enemies
+type EnemyMap =[(Enemy, Location)]
+enemy :: EnemyMap
+enemy = [
+ ("giant", "redRoom"),
+ ("zombie", "blueRoom"),
+ ("spider", "blackRoom")
+ ]
+
+
+type World = (PathMap, LocationMap, Inventory, EnemyMap, Response)
+world :: IO (PathMap, LocationMap, Inventory, EnemyMap, Response)
+world = return (paths, locations, inventory, enemy, "")
 
 main :: IO (String)
 main = do
     putStrLn "\nWelcome to the room game!\n"
     putStrLn instructions
-    play_game ( return (paths, locations, ""))
+    play_game ( return (paths, locations, inventory,enemy, ""))
     return "Goodbye!"
 
+--Game commands
 instructions =
     "Enter commands using single letters or one or two words.\n" ++
     "Available commands are:\n" ++
@@ -59,44 +72,66 @@ instructions =
 
 play_game :: IO (World) -> IO (World)
 play_game world = do
-    (paths, locations, response) <- world
+    (paths, locations, inventory,enemy, response) <- world
     putStrLn response
     putStrLn ""
     do
       putStr "command> "
       command <- getLine
       if command == "quit"
-         then return (paths, locations, "Quitting...")
-         else play_game ( return (do_command command paths locations))
+         then return (paths, locations, inventory, enemy, "Quitting...")
+         else play_game ( return (do_command command paths locations inventory enemy))
 
 move :: Location -> Direction -> PathMap -> Location
 move from direction paths = get (from, direction) paths
 
-do_command :: String -> PathMap -> LocationMap -> World
-do_command "n" paths locations = go "n" paths locations
-do_command "e" paths locations = go "e" paths locations
-do_command "s" paths locations = go "s" paths locations
-do_command "w" paths locations = go "w" paths locations
---do_command "take object" paths locations =
+do_command :: String -> PathMap -> LocationMap -> Inventory ->EnemyMap -> World
+do_command "n" paths locations inventory enemy = go "n" paths locations inventory enemy
+do_command "e" paths locations inventory enemy = go "e" paths locations inventory enemy
+do_command "s" paths locations inventory enemy = go "s" paths locations inventory enemy
+do_command "w" paths locations inventory enemy = go "w" paths locations inventory enemy
+do_command "t" paths locations inventory enemy = takeItem paths locations inventory enemy
+do_command "k" paths locations inventory enemy = killEnemy paths locations inventory enemy
 
-go :: String -> PathMap -> LocationMap -> World
-go direction paths locations = do
+killEnemy :: PathMap -> LocationMap -> Inventory -> EnemyMap -> World
+killEnemy paths locations inventory enemy = do
+  let my_location = get "myself" locations
+  let enemy_location = get "giant" enemy
+  if my_location == enemy_location then do
+    (paths, locations, inventory, enemy, "You killed the giant, so you can go south")
+  else (paths, locations, inventory, enemy, "No enemies to attack")
+
+
+takeItem :: PathMap -> LocationMap -> Inventory -> EnemyMap ->World
+takeItem paths locations inventory enemy = do
+  let my_location = get "myself" locations
+  let item_location = get "sword" locations
+  let item_location_2 = get "diamond" locations
+  if my_location == item_location then do
+    let new_invetory = "sword" : inventory
+    (paths, locations, new_invetory, enemy, "You take the Sword!")
+  else if my_location == item_location_2 then do
+    let new_invetory = "diamond" : inventory
+    (paths, locations, new_invetory, enemy, "You take the Diamond and you Win!")
+  else (paths, locations, inventory, enemy, "Nothing to take!")
+
+go :: String -> PathMap -> LocationMap -> Inventory -> EnemyMap -> World
+go direction paths locations inventory enemy = do
             let my_location = get "myself" locations
             let new_location = move my_location direction paths
             let new_locations = put "myself" new_location locations
             let response = describe new_location new_locations
-            (paths, new_locations, response)
+            (paths, new_locations, inventory, enemy, response)
 
-west_from_blackRoom :: String -> PathMap -> LocationMap -> World
+{-west_from_blackRoom :: String -> PathMap -> LocationMap -> World
 west_from_blackRoom direction paths locations =
     if get "myself" locations == "alive" &&
       "diamond" locations == "whiteRoom" &&
       "myself" locations == "whiteRoom" &&
       "myself" locations == "holding"
       then (paths, put "myself" "whiteRoom" locations, description "whiteRoom1")
-      else go direction paths locations
+      else go direction paths locations-}
 
--- "get" finds the value of a key in a (key, value) list
 get :: Eq a => a -> [(a, String)] -> String
 get value list = case lookup value list of
                      Just result -> result
@@ -110,14 +145,17 @@ put key value list =
 describe :: Location -> LocationMap -> String
 describe new_location locations =
     let here = get "myself" locations
-        greenRoom_status = get "greenRoom" locations
+        greenRoom_status = get "greenRoomAlive" locations
         diamond_location = get "diamond" locations
     in describe_helper here greenRoom_status diamond_location  locations
 
-describe_helper :: Location -> String -> String -> LocationMap -> String
-describe_helper "greenRoom" "dead" "holding" locations = description "greenRoom1"
-describe_helper "whiteRoom" "alive" "holding" locations = description "whiteRoom1"
-
+describe_helper :: Location -> String -> String ->LocationMap -> String
+describe_helper "greenRoom" "alive" _ locations = description "greenRoom"
+describe_helper "whiteRoom" "alive" "holding" locations = description "whiteRoom"
+describe_helper "redRoom" "alive" _ locations = description "redRoom"
+describe_helper "blueRoom" "alive" _ locations = description "blueRoom"
+describe_helper "blackRoom" "alive" _ locations = description "blackRoom"
+describe_helper _ _ _ locations = description "null"
 
 description :: Location -> String
 description "greenRoom" =
@@ -148,4 +186,4 @@ description "whiteRoom" =
 
 description "whiteRoom1" = "Congratulations, you won the game!"
 
-description _ _ = "you can't see anything."
+description _ = "you can't see anything."
